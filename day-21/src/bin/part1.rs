17c97c1394b0c2f9b5c2ecdfe54693f9144e7f62
Kenaz Chan <kenaz.chan@ocrlabs.com>
2023-12-21 17:22:07 +0800

    day-21

---
+++ day-21/src/bin/part1.rs
@@ -0,0 +1,90 @@
+use std::fmt::Debug;
+
+#[derive(Debug)]
+struct Point {
+    x: usize,
+    y: usize,
+    is_start: bool,
+    is_rock: bool,
+    is_reached: bool,
+}
+
+fn main() {
+    // When done, run `cargo run --bin <folder name> to get the output`
+
+    let input = include_str!("./input.txt");
+    let output = process(input);
+    dbg!(output);
+}
+
+fn extract(input: &str) -> Vec<Point> {
+    let garden: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+    let mut points: Vec<Point> = vec![];
+
+    for y in 0..garden.len() {
+        for x in 0..garden[0].len() {
+            let point: Point = match garden[y][x] {
+                'S' => Point { x, y, is_start: true, is_rock: false, is_reached: false },
+                '#' => Point { x, y, is_start: false, is_rock: true, is_reached: false },
+                _ => Point { x, y, is_start: false, is_rock: false, is_reached: false },
+            };
+
+            points.push(point);
+        }
+    }
+
+    points
+}
+
+fn process(input: &str) -> String {
+    // let total_steps: usize = 6;  // For test dataset
+    let total_steps: usize = 64;    // For actual dataset
+    let mut points = extract(input);
+
+    let start = points.iter_mut().find(|p: &&mut Point| p.is_start).unwrap();
+    start.is_reached = true;
+
+    for _ in 0..total_steps {
+        let reached_points: Vec<usize> = points
+            .iter()
+            .enumerate()
+            .filter(|(_, p)| p.is_reached)
+            .map(|(i, _)| i)
+            .collect();
+
+        for index in reached_points {
+            let point = &mut points[index];
+            let neighbors = [
+                (point.x - 1, point.y),
+                (point.x + 1, point.y),
+                (point.x, point.y - 1),
+                (point.x, point.y + 1),
+            ];
+
+            point.is_reached = false;
+
+            for (nx, ny) in neighbors.iter() {
+                if let Some(reached_index) = points.iter().position(|p| !p.is_rock && p.x == *nx && p.y == *ny) {
+                    let reached_point = &mut points[reached_index];
+                    reached_point.is_reached = true;
+                }
+            }
+        }
+    }
+
+    points.iter().filter(|p| p.is_reached).count().to_string()   // Replace with the actual code here
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        // For test, run `cargo test --bin <folder name> to verify the test output`
+
+        let input = include_str!("./input-test.txt");
+        let result = process(input);
+        assert_eq!(result, "16".to_string()); // Replace with the expected test output here
+    }
+}
